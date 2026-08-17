f0ef599c057f4620d1f43e160b5a81a18ae170fa
madeburo <umid.mirzabekov@gmail.com>
2026-08-17 17:07:23 +0500

    chore(next.config): configure standalone output mode

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const isDev = process.env.NODE_ENV === "development";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   images: {
     remotePatterns: [
       {
